74adc72017dda22fe1adb2454d79a6c195d39b54
James Yeung <shunjiey@hotmail.com>
2020-07-12 15:58:02 +0800

    feat: add vditor and docs projects

---
+++ src/Vditor/Components/Editor.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Vditor
+{
+    public partial class Editor : ComponentBase
+    {
+        [Inject] private IJSRuntime JS { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                await JS.InvokeVoidAsync("vditorScript");
+            }
+        }
+    }
+}
