eb2ebf4013c0fefe4f3105fcc938113e89d7cf3c
James Yeung <shunjiey@hotmail.com>
2020-07-12 16:18:48 +0800

    refactor: clean code

--- src/Vditor.Docs.WebAssembly/Program.cs
+++ src/Vditor.Docs.WebAssembly/Program.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Vditor.Docs.WebAssembly
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
